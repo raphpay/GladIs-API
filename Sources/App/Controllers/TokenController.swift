16bbc6b7b2f273e125702647233916aed0f8a44d
raphpay <raphpay.dev@gmail.com>
2024-03-05 10:18:23 +0100

    Feature: Remove all tokens by admin #9

--- Sources/App/Controllers/TokenController.swift
+++ Sources/App/Controllers/TokenController.swift
@@ -20,6 +20,11 @@ struct TokenController: RouteCollection {
         let basicAuthGroup = tokens.grouped(basicAuthMiddleware)
         // Login
         basicAuthGroup.post("login", use: login)
+        // Bearer authentication
+        let tokenAuthMiddleware = Token.authenticator()
+        let guardAuthMiddleware = User.guardMiddleware()
+        let tokenAuthGroup = tokens.grouped(tokenAuthMiddleware, guardAuthMiddleware)
+        tokenAuthGroup.delete("all", use: removeAll)
     }
     
     // MARK: - READ
@@ -71,5 +76,20 @@ struct TokenController: RouteCollection {
                     .delete(force: true, on: req.db)
                     .transform(to: .noContent)
             }
+    }
+    
+    // MARK: - Delete
+    func removeAll(req: Request) async throws -> HTTPResponseStatus {
+        let authUser = try req.auth.require(User.self)
+        
+        guard authUser.userType == .admin else {
+            throw Abort(.badRequest, reason: "User should be admin to delete tokens")
+        }
+        
+        try await Token.query(on: req.db)
+            .all()
+            .delete(force: true, on: req.db)
+        
+        return .noContent
     }
 }
