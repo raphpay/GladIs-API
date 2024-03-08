21f1caad4e40dbd0060e6726c0886549ce844dea
raphpay <raphpay.dev@gmail.com>
2024-03-08 08:32:59 +0100

    Revert "Refactor: Arrange files #11"
    This reverts commit 066910cec0dbffd51ea1dfc4f0fc9d802ab13501.

--- Sources/App/Utils/Extensions/User+Ext.swift
+++ Sources/App/Utils/Extensions/User+Ext.swift
@@ -93,3 +93,33 @@ extension User {
             }
     }
 }
+
+extension Employee {
+    static func generateUniqueUsername(firstName: String, lastName: String, req: Request) async throws -> String {
+        let initialUsername = "\(firstName.lowercased()).\(lastName.lowercased())"
+        
+        let employee = try await Employee.query(on: req.db)
+            .filter(\.$username == initialUsername)
+            .first()
+        
+        if employee == nil {
+            return initialUsername
+        } else {
+            return try await generateUniqueUsernameWithSuffix(initialUsername: initialUsername, suffix: 1, req: req)
+        }
+    }
+    
+    private static func generateUniqueUsernameWithSuffix(initialUsername: String, suffix: Int, req: Request) async throws -> String {
+        let uniqueUsername = "\(initialUsername)-\(suffix)"
+        
+        let employee = try await Employee.query(on: req.db)
+            .filter(\.$username == uniqueUsername)
+            .first()
+        
+        if employee == nil {
+            return uniqueUsername
+        } else {
+            return try await generateUniqueUsernameWithSuffix(initialUsername: initialUsername, suffix: suffix + 1, req: req)
+        }
+    }
+}
