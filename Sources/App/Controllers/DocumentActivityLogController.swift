f8c4e91f4631006a7e20de287c263066407960b4
raphpay <raphpay.dev@gmail.com>
2024-03-24 10:38:08 +0100

    Feature: Add pagination route #21

--- Sources/App/Controllers/DocumentActivityLogController.swift
+++ Sources/App/Controllers/DocumentActivityLogController.swift
@@ -19,6 +19,7 @@ struct DocumentActivityLogController: RouteCollection {
         // Read
         tokenAuthGroup.get(use: getAll)
         tokenAuthGroup.get(":clientID", use: getLogsForClient)
+        tokenAuthGroup.get("paginate", ":clientID", use: getPaginatedLogsForClient)
         // Delete
         tokenAuthGroup.delete(use: removeAll)
     }
@@ -63,6 +64,24 @@ struct DocumentActivityLogController: RouteCollection {
             .query(on: req.db)
             .filter(\.$client.$id == uuid)
             .all()
+    }
+    
+    func getPaginatedLogsForClient(req: Request) async throws -> [DocumentActivityLog] {
+        guard let clientID = req.parameters.get("clientID"),
+              let uuid = UUID(uuidString: clientID) else {
+            throw Abort(.badRequest)
+        }
+        
+        guard let page = req.query[Int.self, at: "page"],
+              let perPage = req.query[Int.self, at: "perPage"] else {
+            throw Abort(.badRequest, reason: "Missing page or perPage query parameters")
+        }
+
+        let paginatedResult = try await DocumentActivityLog.query(on: req.db)
+            .filter(\.$client.$id == uuid)
+            .paginate(PageRequest(page: page, per: perPage))
+
+        return paginatedResult.items
     }
     
     // MARK: - DELETE
