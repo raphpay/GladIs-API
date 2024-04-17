a3a7d7e60aa447c62de22965a63570410c031b19
raphpay <raphpay.dev@gmail.com>
2024-04-17 09:57:13 +0200

    Refactor: Remove unused commented code #28

--- Tests/AppTests/TechnicalDocumentationTabControllerTests/TechnicalDocumentationTabControllerTests.swift
+++ Tests/AppTests/TechnicalDocumentationTabControllerTests/TechnicalDocumentationTabControllerTests.swift
@@ -15,23 +15,6 @@ final class TechnicalDocumentationTabControllerTests: XCTestCase {
     let baseRoute = "api/technicalDocumentationTabs"
     let expectedName = "expectedName"
     let expectedArea = "expectedArea"
-//    let expectedLastName = "expectedLastName"
-//    let expectedPhoneNumber = "0612345678"
-//    let expectedCompanyName = "Acme.inc"
-//    let expectedEmail = "email@test.com"
-//    // Pending User
-//    let expectedPendingUserFirstName = "expectedPendingUserFirstName"
-//    let expectedPendingUserLastName = "expectedPendingUserLastName"
-//    let expectedPendingUserPhoneNumber = "0612345678"
-//    let expectedPendingUserCompanyName = "Acme.inc"
-//    let expectedPendingUserEmail = "expectedPendingUserEmail@test.com"
-//    let expectedPendingUserProducts = "expectedPendingUserProducts"
-//    let expectedPendingUserNumberOfEmployees = 12
-//    let expectedPendingUserNumberOfUsers = 8
-//    let expectedPendingUserSalesAmount: Double = 120000
-//    let expectedPendingUserUsername = "expectedPendingUserUsername"
-//    let expectedPendingUserModuleName = "expectedPendingUserModuleName"
-//    let expectedPendingUserModuleIndex = 1
     let expectedAdminUsername = "expectedAdminUsername"
     
     override func setUp() async throws {
