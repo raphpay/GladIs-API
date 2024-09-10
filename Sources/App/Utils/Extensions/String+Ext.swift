074e09249d24c40b8704f994b304c0a6721d57b6
raphpay <raphaelpayet@icloud.com>
2024-09-10 08:47:25 +0400

    Feature: Remove email and phone verification if not needed in PUT method #35

--- Sources/App/Utils/Extensions/String+Ext.swift
+++ Sources/App/Utils/Extensions/String+Ext.swift
@@ -5,7 +5,7 @@
 //  Created by Raphaël Payet on 09/09/2024.
 //
 
-import Foundation
+import Vapor
 
 extension String {
     func removeAccents() -> String {
@@ -15,5 +15,34 @@ extension String {
     
     func removeWhitespaces(with string: String = "") -> String {
         self.replacingOccurrences(of: " ", with: string)
+    }
+    
+    func validateEmail() throws {
+        let regex = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
+        let isValid = NSPredicate { input, _ in
+            guard let input = input as? String else {
+                return false
+            }
+            
+            return input.range(of: regex, options: .regularExpression) != nil
+        }.evaluate(with: self)
+        
+        guard isValid else {
+            throw Abort(.badRequest, reason: "badRequest.email.invalid")
+        }
+    }
+    
+    func validatePhoneNumber() throws {
+        let regex = #"^(0|\+33|0033)[1-9]([-. ]?[0-9]{2}){4}$"#
+        let isValid = NSPredicate { input, _ in
+            guard let input = input as? String else {
+                return false
+            }
+            return input.range(of: regex, options: .regularExpression) != nil
+        }.evaluate(with: self)
+        
+        guard isValid else {
+            throw Abort(.badRequest, reason: "badRequest.phoneNumber.invalid")
+        }
     }
 }
