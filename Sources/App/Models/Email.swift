13e133d9a202e7b8c86fe15aae53e6555538fdc5
raphpay <raphaelpayet@icloud.com>
2024-09-30 17:15:52 +0200

    Feature: Add email support via SendGrid #39

---
+++ Sources/App/Models/Email.swift
@@ -0,0 +1,15 @@
+//
+//  Email.swift
+//  
+//
+//  Created by Raphaël Payet on 30/09/2024.
+//
+
+import Fluent
+import Vapor
+import SendGridKit
+
+struct Email: Content {
+    // See EmailDTO.swift
+}
+
