4c466a08676b8c12500e7cd5c8ccf74e660b4a49
raphpay <raphpay.dev@gmail.com>
2025-01-23 14:42:13 +0400

    Tests: Start testing #98

---
+++ Tests/AppTests/QuestionnaireControllerTests/QuestionnaireControllerTests+CREATE.swift
@@ -0,0 +1,45 @@
+//
+//  QuestionnaireControllerTests+CREATE.swift
+//  GladIs-API
+//
+//  Created by Raphaël Payet on 23/01/2025.
+//
+
+@testable import App
+import XCTVapor
+import Fluent
+import Vapor
+
+// MARK: - Create
+extension QuestionnaireControllerTests {
+    func test_Create_Succeed() async throws {
+//        TODO: Remove response count and sentAt from input
+        let client = try await  UserControllerTests().createExpectedUser(userType: .client, on: app.db)
+        let clientID = try client.requireID()
+        let adminID = try admin.requireID()
+        let input = Questionnaire.Input(title: expectedTitle,
+                                        fields: expectedFields,
+                                        adminID: adminID,
+                                        sentAt: nil,
+                                        responseCount: 0,
+                                        clientIDs: [clientID]
+        )
+        try await app.test(.POST, baseURL, beforeRequest: { req in
+            req.headers.bearerAuthorization = BearerAuthorization(token: token.value)
+            try req.content.encode(input)
+        }, afterResponse: { res async in
+            // Then
+            XCTAssertEqual(res.status, .ok)
+            do {
+                let questionnaire = try res.content.decode(Questionnaire.self)
+                XCTAssertEqual(questionnaire.title, expectedTitle)
+                XCTAssertEqual(questionnaire.fields.count, expectedFields.count)
+                
+                let qID = try questionnaire.requireID()
+                let qRecipients = try await QuestionnaireRecipient.query(on: app.db).all()
+                XCTAssertEqual(qRecipients[0].$client.id, clientID)
+                XCTAssertEqual(qRecipients[0].$questionnaire.id, qID)
+            } catch {}
+        })
+    }
+}
---
+++ Tests/AppTests/QuestionnaireControllerTests/QuestionnaireControllerTests.swift
@@ -0,0 +1,45 @@
+//
+//  QuestionnaireControllerTests.swift
+//
+//
+//  Created by Raphaël Payet on 17/04/2024.
+//
+
+@testable import App
+import XCTVapor
+import Fluent
+
+final class QuestionnaireControllerTests: XCTestCase {
+    
+    var app: Application!
+    let baseURL = "api/questionnaires"
+    var admin: User!
+    var adminID: User.IDValue!
+    var token: Token!
+    
+    override func setUp() async throws {
+        try await super.setUp()
+        app = Application(.testing)
+        try! await configure(app)
+        admin = try await UserControllerTests().createExpectedAdmin(on: app.db)
+        adminID = try admin.requireID()
+        token = try await Token.create(for: admin, on: app.db)
+    }
+    
+    override func tearDown() async throws {
+        try await User.deleteAll(on: app.db)
+        adminID = nil
+        try await token.delete(force: true, on: app.db)
+        
+        // Required
+        app.shutdown()
+        try await super.tearDown()
+    }
+    
+    // Expected Properties
+    let expectedTitle = "Test Questionnaire"
+    let expectedFields = [Questionnaire.QField(key: "Test Field 1", index: 1),
+                          Questionnaire.QField(key: "Test Field 2", index: 2)]
+    
+}
+
