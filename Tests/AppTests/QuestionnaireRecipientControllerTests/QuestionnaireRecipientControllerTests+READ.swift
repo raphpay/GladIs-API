b9ee38462653f9890bdc4e6ba4e22ef217a143d6
raphpay <raphpay.dev@gmail.com>
2025-01-23 16:48:36 +0400

    Tests: test get methods #99

--- Tests/AppTests/QuestionnaireRecipientControllerTests/QuestionnaireRecipientControllerTests+READ.swift
+++ Tests/AppTests/QuestionnaireRecipientControllerTests/QuestionnaireRecipientControllerTests+READ.swift
@@ -46,3 +46,75 @@ extension QuestionnaireRecipientControllerTests {
         })
     }
 }
+
+// MARK: - Get Questionnaire
+extension QuestionnaireRecipientControllerTests {
+    func test_GetQuestionnaire_Succeed() async throws {
+        // Given
+        let qRecipient = try await QuestionnaireRecipientControllerTests().createExpectedQRecipient(
+            qID: qID,
+            clientID: clientID,
+            on: app.db
+        )
+        
+        let expectedRecipientID = try qRecipient.requireID()
+        
+        try await app.test(.GET, "\(baseURL)/questionnaire/\(expectedRecipientID)", beforeRequest: { req in
+            // Add the Bearer token for authentication
+            req.headers.bearerAuthorization = BearerAuthorization(token: token.value)
+        }, afterResponse: { res async in
+            // Then
+            XCTAssertEqual(res.status, .ok)
+            
+            // Decode the response as a `Questionnaire`
+            do {
+                let questionnaire = try res.content.decode(Questionnaire.self)
+                
+                // Check if the questionnaire matches the expected data
+                XCTAssertEqual(questionnaire.id, qID)
+                
+                // Verify the recipient's status has been updated in the database
+                guard let updatedRecipient = try await QuestionnaireRecipient.find(expectedRecipientID, on: app.db) else {
+                    XCTFail("QuestionnaireRecipient not found in the database")
+                    return
+                }
+                XCTAssertEqual(updatedRecipient.status, .viewed)
+            } catch {
+                XCTFail("Failed to decode response or validate data: \(error.localizedDescription)")
+            }
+        })
+    }
+    
+    func test_GetQuestionnaire_WithInexistantRecipient_Fails() async throws {
+        let expectedRecipientID = UUID()
+        
+        try await app.test(.GET, "\(baseURL)/questionnaire/\(expectedRecipientID)", beforeRequest: { req in
+            // Add the Bearer token for authentication
+            req.headers.bearerAuthorization = BearerAuthorization(token: token.value)
+        }, afterResponse: { res async in
+            // Then
+            XCTAssertEqual(res.status, .notFound)
+            XCTAssertTrue(res.body.string.contains("notFound.questionnaireRecipient"))
+        })
+    }
+    
+    func test_GetQuestionnaire_WithInexistantQuestionnaire_Fails() async throws {
+        // Given
+        let qRecipient = try await QuestionnaireRecipientControllerTests().createExpectedQRecipient(
+            qID: UUID(),
+            clientID: clientID,
+            on: app.db
+        )
+        
+        let expectedRecipientID = try qRecipient.requireID()
+        
+        try await app.test(.GET, "\(baseURL)/questionnaire/\(expectedRecipientID)", beforeRequest: { req in
+            // Add the Bearer token for authentication
+            req.headers.bearerAuthorization = BearerAuthorization(token: token.value)
+        }, afterResponse: { res async in
+            // Then
+            XCTAssertEqual(res.status, .notFound)
+            XCTAssertTrue(res.body.string.contains("notFound.questionnaire"))
+        })
+    }
+}
